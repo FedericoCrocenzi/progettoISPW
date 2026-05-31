0ef838f005740b8f31668a40542f1231a33e6715
FedericoCrocenzi <federicocrocenzi@gmail.com>
2026-05-31 19:36:28 +0200

    Fix system.out and system.err

---
+++ src/main/java/it/ispw/project/graphicControllerCLI/CLIPrinter.java
@@ -0,0 +1,28 @@
+package it.ispw.project.graphicControllerCLI;
+
+public final class CLIPrinter {
+
+    private CLIPrinter() {
+        // Utility class.
+    }
+
+    @SuppressWarnings("java:S106")
+    public static void print(String message) {
+        System.out.print(message);
+    }
+
+    @SuppressWarnings("java:S106")
+    public static void println(String message) {
+        System.out.println(message);
+    }
+
+    @SuppressWarnings("java:S106")
+    public static void println() {
+        System.out.println();
+    }
+
+    @SuppressWarnings("java:S106")
+    public static void printf(String format, Object... args) {
+        System.out.printf(format, args);
+    }
+}
